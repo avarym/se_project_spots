0a6e364577c31880c97bcfa42afb78e997f218ad
Avary Marsh <avarymarsh12@gmail.com>
2025-02-27 14:17:57 -0600

    add js file and card array/objects

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards=[
+    {
+        name: "Brown turtle in water during daytime",
+        link: "https://unsplash.com/photos/brown-turtle-in-water-during-daytime-M8xxVih_V_U?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+    },
+    {
+        name: "A person in an orange jacket is planting flowers",
+        link: "https://unsplash.com/photos/a-person-in-an-orange-jacket-is-planting-flowers-cz9vIzWyqUc"
+    },
+    {
+        name: "planet earth first sinage sticked in gray post outdoors",
+        link: "https://unsplash.com/photos/planet-earth-first-signage-sticked-in-gray-post-outdoors-D0xQQsZovws"
+    },
+    {
+        name: "people in yellow jacket and black backpack",
+        link: "https://unsplash.com/photos/people-in-yellow-jacket-and-black-backpack-FyCjvyPG9Pg"
+    },
+    {
+        name: "person holding there is no planet b poster",
+        link: "https://unsplash.com/photos/person-holding-there-is-no-planet-b-poster-ycW4YxhrWHM"
+    },
+    {
+        name: "Blue and white plastic pack lot",
+        link: "https://unsplash.com/photos/blue-and-white-plastic-pack-lot-ywVgG0lDbOk"
+    }
+];
