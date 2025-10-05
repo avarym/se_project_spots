d963e1b90af47a0c83e0120f7cd7b4b7e1eee4b4
Avary Marsh <avarymarsh12@gmail.com>
2025-10-05 14:00:20 -0500

    add like/unlike button functionality on cards

--- scripts/index.js
+++ scripts/index.js
@@ -84,5 +84,11 @@ const cardElementImage = cardElement.querySelector(".card__img");
 cardElementImage.alt = `${data.name}`;
 cardElementImage.src = `${data.link}`;
 cardElementTitle.textContent = `${data.name}`;
+
+const cardLikeBtn = cardElement.querySelector(".card__like-btn");
+cardLikeBtn.addEventListener("click", function(evt) {
+  evt.target.classList.toggle("card__like-btn_active");
+});
+
 return cardElement;
 }
